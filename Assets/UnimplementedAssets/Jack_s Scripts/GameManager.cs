9c864074c9296909730405f1c42f52fa112ce348
Christoph5782 <47129859+Christoph5782@users.noreply.github.com>
2020-05-07 16:32:33 -0500

    Added Jack's Scripts & Sounds/Ali's Enemy Sprites

---
+++ Assets/UnimplementedAssets/Jack_s Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    public GameObject youWinText;
+    public GameObject youLoseText;
+    public GameObject returnMenuButton;
+    public GameObject tileScreen;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        youWinText.SetActive(false);
+        youLoseText.SetActive(false);
+        tileScreen.SetActive(false);
+        returnMenuButton.SetActive(false);
+    }
+
+    public void Win()
+    {
+        youWinText.SetActive(true);
+        returnMenuButton.SetActive(true);
+        tileScreen.SetActive(true);
+
+    }
+
+    public void Lose()
+    {
+        youLoseText.SetActive(true);
+        returnMenuButton.SetActive(true);
+        tileScreen.SetActive(true);
+    }
+}
